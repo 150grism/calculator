f6f0f9ea03ceb736dbc2e9690ebda00ea731fcb8
Grigory Moshkevich <ilcostuff@gmail.com>
2017-10-08 09:06:06 +0300

    .

---
+++ script.js
@@ -0,0 +1,65 @@
+var button = document.querySelectorAll('button');
+var displayInput = document.getElementById('display-input');
+var displayResult = document.getElementById('display-result');
+// console.log('butt');
+
+
+var text = '';
+
+document.onclick = function () {
+  let lastCharacter = text.charAt(text.length - 1);
+  let secondLastCharacter = text.charAt(text.length - 2);
+  if (event.target.tagName == 'BUTTON') {
+    button = event.target;
+    val = event.target.value;
+    if (val === 'backspace') {
+      text = text.slice(0, -1);
+    } else if (val === 'clear') {
+      text = '';
+    } else if (['+','-','*','/'].indexOf(val) > -1 && ['+','-','*','/'].indexOf(lastCharacter) > -1) {
+      text = text.slice(0, -1);
+      text += val;
+    } else if (val === '%' && ['+','-','*','/'].indexOf(lastCharacter) > -1) {
+      if (!secondLastCharacter === '%') {
+        text = text.slice(0, -1);
+        text += val;
+      } else {
+        text = text.slice(0, -1);
+      }
+    } else if (['+','-','*','/','%'].indexOf(lastCharacter) > -1 && val === lastCharacter) {
+    } else if (val !== '=') {
+      text += val;
+    } 
+    displayInput.innerHTML = text;
+    console.log(text);
+
+    if (val === '=') {
+      SchitaiBatika();
+    }
+    // console.log(openParenthesesCount);
+  }
+
+  SchitaiBatika = () => {
+    console.clear();
+    // console.log(eval(text));
+    for (let i = 0; i < text.length; i++) {
+      if ( text.charAt(i) === '%') {
+        console.log(i);
+        let k = 0, j;
+        let percentText;
+        for (j = i-1; ['+','-','*','/','%'].indexOf(text[j]) < 0; j--) { 
+          k++;
+        }
+        console.log(j);
+        percentText = text.substring(i-k, i+1);
+        console.log(percentText);
+        percentText = percentText.slice(0, -1);
+        console.log(percentText);
+        text = text.replace(percentText + '%',parseFloat(percentText)/100)
+        console.log(text);
+      }
+    }
+    result = parseFloat(eval(text).toFixed(2));
+    displayResult.innerHTML = '=' + result;  
+  }
+}
