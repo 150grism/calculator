49f2693dcde85085224faeec7f3dbb93e58cf9fb
Grigory Moshkevich <ilcostuff@gmail.com>
2017-10-16 15:58:43 +0300

    easy dot management first commit

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ var cv, qw;
 var d = new Date();
 var timer;
 
+//таймер для предупреждения о превышении допустимого количества символов
+function endAndStartTimer() {
+  window.clearTimeout(timer);
+  //var millisecBeforeRedirect = 10000; 
+  timer = window.setTimeout(function() {upToAlert.style.display = 'none';}, 3000); 
+}
+
+//время в строке состояния
 var hours = d.getHours(), minutes = d.getMinutes();
 if (hours < 10) {
   hours = '0' + hours;
@@ -21,55 +29,71 @@ if (hours < 10) {
 if (minutes < 10) {
   minutes = '0' + minutes;
 } 
-
 time.innerHTML = hours + ':' + minutes;
 
+//функционал кнопок
 document.onclick = function () {
 
   if (event.target.tagName == 'BUTTON') {
     theButton = event.target;
     val = event.target.value;
-    // if (typeof val = 'number')
+    //при превышении 35 символов, вывести предупреждение (исчезнет по таймеру) и удалить последний введенный символ
     if (text.replace(/,/g,'').length > 35) {
       upToAlert.style.display = 'block';
       endAndStartTimer();
       // setTimeout(function() {upToAlert.style.display = 'none';}, 3000);
       text = text.slice(0, -1);
-    } else {
-      upToAlert.style.display = 'none';
     }
+    //если на момент нажатия кнопки на экране был выведен результат вычесления, стереть результат и стереть инпут юзера
     if (displayResult.innerHTML !== '') {
       text = '';
       displayResult.innerHTML = '';
     }
+    //если нажата кнопка "backspace" (если значение кнопки = "backspace"), 
+    //стереть последний символ и еще стереть запятую, если она повиснет в конце инпута
     if (val === 'backspace') {
       if (lastCharacter(2) === ',') {
+        if (text.lastIndexOf('.') + 1 > text.length - 2) {
+          dot = false;
+        }
         text = text.slice(0, -2);
       } else {
+        if (text.lastIndexOf('.') + 1 > text.length - 1) {
+          dot = false;
+        }
         text = text.slice(0, -1);
       }
+    //если кнопка - "AC", стереть результат + стереть весь инпут
     } else if (val === 'clear') {
       text = '';
       displayResult.innerHTML = '';
-    } else if (['+','-','\u00D7','\u00F7'].indexOf(val) > -1 && ['+','-','\u00D7','\u00F7'].indexOf(lastCharacter(1)) > -1) {
+    //если кнопка выполняет одну из операций (сложение, вычитание, деление, умножение), в то время как 
+    //последний символ в инпуте = одна из этих операций, заменить этот последний символ новой операцией
+    } else if (['+', '-', '\u00D7', '\u00F7'].indexOf(val) > -1 && ['+', '-', '\u00D7', '\u00F7'].indexOf(lastCharacter(1)) > -1) {
       text = text.slice(0, -1);
       text += val;
+    //если кнопка - "точка", и последний символ инпута - %, 
     } else if (val === '.' && lastCharacter(1) === '%') {
-      for (cv = text.length - 1; ['+','-','	\u00D7','\u00F7','%','.'].indexOf(text.charAt(cv)) < 0 && cv > 0; cv--) {console.log(cv);}
+      // ... начиная с конца, ищем позицию последнего символа, который является операцией. Это будет, фактически, конец предпоследнего числа в инпуте
+      for (cv = text.length - 1; ['+', '-', '\u00D7', '\u00F7', '%', '.'].indexOf(text.charAt(cv)) < 0 && cv > 0; cv--) {console.log(cv);}
+      // ... находим позицию последней точки в инпуте. 
       qw = text.lastIndexOf('.');
-      if ((qw >= cv && dot === false) || qw === -1) {
+      // ... если точка в инпуте позже предпоследнего числа в инпуте, значит точка находится где-то в последнем числе.
+      //тогда 
+      // if ((qw >= cv && dot === false) || qw === -1) {
+        //dot = true - значит, что точка в числе уже есть - значит новую точку в число ставить запрещено
         dot = true;
-        text = text + '	\u00D7' + '0' + val;
-      }
-    } else if (['\u00D7','\u00F7','%'].indexOf(val) > -1 && text.length === 0) {
-    } else if (val === '%' && ['+','-','\u00D7','\u00F7'].indexOf(lastCharacter(1)) > -1) {
+        text = text + '\u00D7' + '0' + val;
+      // }
+    } else if (['\u00D7', '\u00F7', '%'].indexOf(val) > -1 && text.length === 0) {
+    } else if (val === '%' && ['+', '-', '\u00D7', '\u00F7'].indexOf(lastCharacter(1)) > -1) {
       if (lastCharacter(2) !== '%') {
         text = text.slice(0, -1);
         text += val;
       } else {
         text = text.slice(0, -1);
       }
-    } else if (['+','-','\u00D7','\u00F7','%'].indexOf(lastCharacter(1)) > -1 && val === lastCharacter(1)) {
+    } else if (['+', '-', '\u00D7', '\u00F7', '%'].indexOf(lastCharacter(1)) > -1 && val === lastCharacter(1)) {
     } else if (val !== '=' && val !== '.' && theButton.className !== 'numbers') {
       text += val;
     }
@@ -80,15 +104,15 @@ document.onclick = function () {
         } else {
           text += val;
         }
-        for (cv = text.length - 1; ['+','-','\u00D7','\u00F7','%','.'].indexOf(text.charAt(cv)) < 0 && cv > 0; cv--) {console.log(cv);}
-        text = text.slice(0, cv) + text.slice(cv).replace(/,/g,'');
+        for (cv = text.length - 1; ['+', '-', '\u00D7', '\u00F7', '%', '.'].indexOf(text.charAt(cv)) < 0 && cv > 0; cv--) {console.log(cv);}
+        text = text.slice(0, cv) + text.slice(cv).replace(/,/g, '');
         console.log(text);
         var zx = 0;
         for (qw = text.length - 1; qw > cv; qw--) {
           // console.log(qw + ' ' + text.charAt(qw) + ' ' + zx);
           zx++;
           zx = zx % 3;
-          if (zx === 0 && ['+','-','\u00D7','\u00F7','%'].indexOf(text.charAt(qw-1)) < 0) {
+          if (zx === 0 && ['+', '-', '\u00D7', '\u00F7', '%'].indexOf(text.charAt(qw - 1)) < 0) {
             text = text.slice(0, qw) + "," + text.slice(qw);
             // console.log('-> ' + text);
           }
@@ -97,7 +121,7 @@ document.onclick = function () {
         if (qw >= cv) {
           dot = true;
           console.log(qw + ' --- ' + cv);
-          text = text.slice(0, qw) + text.slice(qw).replace(/,/g,'');
+          text = text.slice(0, qw) + text.slice(qw).replace(/,/g, '');
         } else {dot = false;}
       } else {
         if (dot === false) {
@@ -137,9 +161,9 @@ document.onclick = function () {
   }
 
   SchitaiBatika = () => {
-    text = text.replace(/,/g,'');
-    text = text.replace(/\u00F7/g,'/');
-    text = text.replace(/\u00D7/g,'*');
+    text = text.replace(/,/g, '');
+    text = text.replace(/\u00F7/g, '/');
+    text = text.replace(/\u00D7/g, '*');
     // console.clear();
     // console.log(eval(text));
     for (let i = 0; i < text.length; i++) {
@@ -147,20 +171,20 @@ document.onclick = function () {
         console.log('i:' + i);
         let k = 0, j;
         let percentText;
-        for (j = i-1; ['+','-','*','/','%'].indexOf(text[j]) < 0 && j >= 0; j--) { 
+        for (j = i-1; ['+', '-', '*', '/', '%'].indexOf(text[j]) < 0 && j >= 0; j--) { 
           k++;
         }
         console.log('j:' + j);
         console.log('k:' + k);
-        percentText = text.substring(i-k, i+1);
+        percentText = text.substring(i - k, i + 1);
         console.log(percentText);
         percentText = percentText.slice(0, -1);
         console.log(percentText);
-        text = text.replace(percentText + '%',parseFloat(percentText)/100)
+        text = text.replace(percentText + '%',parseFloat(percentText) / 100);
         console.log(text);
       }
     }
-    while (['+','-','*','/'].indexOf(lastCharacter(1)) > -1) {
+    while (['+', '-', '*', '/'].indexOf(lastCharacter(1)) > -1) {
       text = text.slice(0, -1);
       console.log(text);
     }
@@ -182,20 +206,14 @@ document.onclick = function () {
       }
     result = '=' + result; 
     if (fSize = 1) {
-      result = result.slice(0,16);
+      result = result.slice(0, 16);
     }
     if (fSize = 2) {
-      result = result.slice(0,17);
+      result = result.slice(0, 17);
     }
     if (fSize = 3) {
-      result = result.slice(0,20);
+      result = result.slice(0, 20);
     }
     displayResult.innerHTML = result;  
   }
-}
-
-function endAndStartTimer() {
-  window.clearTimeout(timer);
-  //var millisecBeforeRedirect = 10000; 
-  timer = window.setTimeout(function() {upToAlert.style.display = 'none';}, 3000); 
 }
